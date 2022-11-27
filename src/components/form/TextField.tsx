04a7bb33eb0c1ceb3bb8678e8e29fe576253f29d
유다연 <udayeon@naver.com>
2022-11-27 19:56:18 +0900

    add TextField component (#6)
    * add TextField component
    
    * add TextArea component
    
    * fix TextField component
    
    * component test

---
+++ src/components/form/TextField.tsx
@@ -0,0 +1,72 @@
+import { Typography, useTextStyle } from "components/text";
+import { colors } from "constants/color";
+import { InputHTMLAttributes } from "react";
+import styled from "styled-components";
+import { SizeType } from "types";
+import { convertPixelValue } from "utils";
+
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  typography?: Typography;
+  width?: SizeType;
+  height?: SizeType;
+  borderColor?: string;
+  borderRadius?: SizeType;
+  textColor?: string;
+  placeholderColor?: string;
+}
+
+export function TextField({
+  typography = Typography.Text_16,
+  width = 520,
+  height = 48,
+  borderColor = colors.borderGrey,
+  borderRadius = 8,
+  textColor = colors.black,
+  placeholderColor = colors.borderGrey,
+  ...props
+}: Props) {
+  const textStyle = useTextStyle({ typography: typography, color: textColor });
+  return (
+    <StyledTextField
+      {...props}
+      width={width}
+      height={height}
+      borderColor={borderColor}
+      borderRadius={borderRadius}
+      textColor={textStyle.color}
+      placeholderColor={placeholderColor}
+    />
+  );
+}
+
+const StyledTextField = styled.input<{
+  width: SizeType;
+  height: SizeType;
+  borderColor: string;
+  borderRadius: SizeType;
+  textColor: string;
+  placeholderColor: string;
+}>`
+  ${(p) => `width: ${convertPixelValue(p.width)};`}
+  ${(p) => `height: ${convertPixelValue(p.height)};`}
+  
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-top: 10px;
+  padding-bottom: 12px;
+
+  ${(p) => `color: ${p.textColor};`}
+
+  ${(p) => `border: 1px solid ${p.borderColor};`}
+  ${(p) => `border-radius: ${convertPixelValue(p.borderRadius)};`}
+
+  ::placeholder {
+    ${(p) => `color: ${p.placeholderColor};`}/* font-size: 0.8rem; */
+  }
+
+  :focus {
+    outline: none !important;
+    border-color: ${colors.green};
+    box-shadow: 0 0 10px ${colors.green};
+  }
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { Button } from "components/button";
+import { TextArea } from "components/form";
+import { TextField } from "components/form/TextField";
 import { Text } from "components/text/Text";
 import Head from "next/head";
 import Image from "next/image";
@@ -6,6 +8,7 @@ import { useState } from "react";
 import styles from "styles/Home.module.css";
 
 export default function Home() {
+  const [value, setValue] = useState<string>("");
   return (
     <div className={styles.container}>
       <Text>안녕</Text>
@@ -16,6 +19,21 @@ export default function Home() {
       >
         로그인
       </Button>
+      <TextField
+        placeholder="텍스트 입력"
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      />
+      <TextArea
+        placeholder="hi"
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      />
+      <Text maxLength={10}>{value}</Text>
     </div>
   );
 }
