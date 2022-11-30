3d8fe369c7f1baa8511b34fdfcb309633a6bfe2a
유다연 <udayeon@naver.com>
2022-11-30 15:10:39 +0900

    Chore/development environment setting (#13)
    * prettier 설정
    
    * index.js 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,47 @@
-import { Button } from "components/button";
-import { TextArea } from "components/form";
-import { TextField } from "components/form/TextField";
-import { Img } from "components/img";
-import { Text } from "components/text/Text";
-import Head from "next/head";
-import Image from "next/image";
-import { useState } from "react";
-import styles from "styles/Home.module.css";
+import {Button} from 'components/button';
+import {CheckBox, TextArea, TextField} from 'components/form';
+import {Img} from 'components/img';
+import {Text} from 'components/text';
+import Head from 'next/head';
+import Image from 'next/image';
+import {useState} from 'react';
+import styles from 'styles/Home.module.css';
 
 export default function Home() {
-  const [value, setValue] = useState<string>("");
+  const [value, setValue] = useState<string>('');
+  const [isChecked, setIsChecked] = useState<boolean>(false);
   return (
     <div className={styles.container}>
       <Text>안녕</Text>
       <Button
         onClick={() => {
-          console.log("hi");
-        }}
-      >
+          console.log('hi');
+        }}>
         로그인
       </Button>
       <TextField
         placeholder="텍스트 입력"
         value={value}
-        onChange={(e) => {
+        onChange={e => {
           setValue(e.target.value);
         }}
       />
       <TextArea
         placeholder="hi"
         value={value}
-        onChange={(e) => {
+        onChange={e => {
           setValue(e.target.value);
         }}
       />
       <Text maxLength={10}>{value}</Text>
-      <Img src={"/assets/icons/ic_Article.png"} />
+      <Img src={'/assets/icons/ic_Article.png'} />
+      <CheckBox
+        type="circle"
+        checked={isChecked}
+        onClick={() => {
+          setIsChecked(!isChecked);
+        }}
+      />
     </div>
   );
 }
